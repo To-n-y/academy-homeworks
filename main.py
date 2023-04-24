b46d3ef00773903c83b27a360237a1289c845a1d
Tony <realvitl@yandex.ru>
2023-04-24 18:55:40 +0300

    add websockets

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 from routers.routerfriends import routerFriends
 from routers.routeruser import routerUser
@@ -7,3 +7,11 @@ app = FastAPI()
 
 app.include_router(routerUser, prefix="/user")
 app.include_router(routerFriends, prefix="/friends")
+
+
+@app.websocket("/ws")
+async def websocket(websocket: WebSocket):
+    await websocket.accept()
+    data = await websocket.receive_text()
+    await websocket.send_text(f"You send: {data}")
+    await websocket.close()
