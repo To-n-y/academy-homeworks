d08748591c848e292c35dbf56d1a55da7bd3580d
Tony <realvitl@yandex.ru>
2023-04-24 21:02:02 +0300

    add chat room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
 
 from routers.routerfriends import routerFriends
 from routers.routeruser import routerUser
@@ -16,3 +17,21 @@ async def websocket(websocket: WebSocket):
 
 app.include_router(routerFriends, prefix="/friends")
 app.include_router(routerUser, prefix="/user")
+
+chat_rooms = {}
+
+
+@app.websocket("/ws/{chat_name}")
+async def websocket_endpoint(websocket: WebSocket, chat_name: str):
+    await websocket.accept()
+    if chat_name not in chat_rooms:
+        chat_rooms[chat_name] = []
+    chat_rooms[chat_name].append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for ws in chat_rooms[chat_name]:
+                if ws != websocket:
+                    await ws.send_text(data)
+    except WebSocketDisconnect:
+        chat_rooms[chat_name].remove(websocket)
