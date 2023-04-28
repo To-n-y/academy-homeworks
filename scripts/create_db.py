6cd848a92c62ce54b88b0caebf4d5f742170929a
Tony <realvitl@yandex.ru>
2023-04-28 22:21:31 +0300

    add script to create db

---
+++ scripts/create_db.py
@@ -0,0 +1,22 @@
+import os
+import sqlite3
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+db_path = os.path.join(parent_dir, "mydatabase.db")
+conn = sqlite3.connect(db_path)
+
+conn.execute(
+    """
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    age INTEGER,
+    about TEXT,
+    password TEXT
+);
+"""
+)
+
+conn.close()
