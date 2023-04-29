1d66e56aac57f529bd1b23e0141000b8e98393c7
Tony <realvitl@yandex.ru>
2023-04-29 21:48:52 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret")
+DATABASE_URL = os.getenv("DATABASE_URL", "secret.db")
