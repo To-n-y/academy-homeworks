9d282499f15a2de3eeac00d4109713739db5f58e
Tony <realvitl@yandex.ru>
2023-05-08 23:50:33 +0300

    update config

--- app/config.py
+++ app/config.py
@@ -7,4 +7,5 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret")
-DATABASE_URL = os.getenv("DATABASE_URL", "secret.db")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "name")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
